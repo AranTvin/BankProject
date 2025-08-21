f0a4597695622a0090672654f38b7cba12deda17
Vlad <gromov.vlv@yandex.ru>
2025-08-22 00:03:16 +0300

    mistakes are fixed

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,7 @@ from mask import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
-    """Обрабатывает информацию о картах и счетах и маскирует их номера"""
+    """Обрабатывает информацию о картах и счетах пользователя и маскирует их номера"""
     account_type_list = []
     for i in account_card:
         if not i.isdigit():
@@ -18,5 +18,5 @@ def mask_account_card(account_card: str) -> str:
 
 
 def get_date(date_time: str) -> str:
-    """Преобразует строку с датой в определенном формате в формат 'ДД.ММ.ГГГГ'"""
+    """Преобразует строку с датой в определенном формате в формат ДД.ММ.ГГГГ"""
     return f"{date_time[8:10]}.{date_time[5:7]}.{date_time[0:4]}"
