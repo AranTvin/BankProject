6e583711552b332f9d7df5204beb72a15c97b62c
Vlad <gromov.vlv@yandex.ru>
2025-09-03 22:05:53 +0300

    filter_by_state and sort_by_state created

--- src/processing.py
+++ src/processing.py
@@ -1,1 +1,8 @@
 def filter_by_state(operations_list, state="EXECUTED"):
+    """Фильтрует операции по статусу"""
+    return [i for i in operations_list if i["state"] == state]
+
+
+def sort_by_date(operations_list, reverse=True):
+    """Сортирует операции по дате"""
+    return sorted(operations_list, key=lambda x: x['date'], reverse=reverse)
