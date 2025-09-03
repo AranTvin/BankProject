5c4c4c217a7ef79dc9417ca9e4670efd15d0cb9d
Vlad <gromov.vlv@yandex.ru>
2025-09-03 22:57:57 +0300

    processing.py finished

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filter_by_state(operations_list, state="EXECUTED"):
+def filter_by_state(operations_list: str, state: str="EXECUTED") -> list:
     """Фильтрует операции по статусу"""
     return [i for i in operations_list if i["state"] == state]
 
 
-def sort_by_date(operations_list, reverse=True):
+def sort_by_date(operations_list: list, reverse: bool=True) -> list:
     """Сортирует операции по дате"""
-    return sorted(operations_list, key=lambda x: x['date'], reverse=reverse)
+    return sorted(operations_list, key=lambda x: x["date"], reverse=reverse)
