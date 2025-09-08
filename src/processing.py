ba64925e53e020e7a50ded6c3b9c851dc282b2fd
Vlad <gromov.vlv@yandex.ru>
2025-09-08 20:58:18 +0300

    copylist 'transactions_sorted' added, annotations and variables corrected

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,14 @@
 from widget import get_date
 
 
-def filter_by_state(transactions: list, status: str = "EXECUTED") -> list:
+def filter_by_state(transactions: list[dict], status: str = "EXECUTED") -> list[dict]:
     """Фильтрует операции по статусу"""
-    return [i for i in transactions if i["state"] == status]
+    return [transaction for transaction in transactions if transaction["state"] == status]
 
 
-def sort_by_date(transactions: list, reverse: bool = True) -> list:
+def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
     """Сортирует операции по дате"""
-    for i in transactions:
-        i["date"] = get_date(i["date"])
-    return sorted(transactions, key=lambda x: x["date"], reverse=reverse)
+    transactions_sorted = sorted(transactions, key=lambda x: x["date"], reverse=reverse)
+    for transaction in transactions_sorted:
+        transaction["date"] = get_date(transaction["date"])
+    return transactions_sorted
