514fbbd24cbfca48d29e065c4f198edd0e2144cd
Vlad <gromov.vlv@yandex.ru>
2025-10-11 14:44:36 +0300

    fixtures created

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+@pytest.fixture()
+def masked_account():
+    return "**7890"
+
+@pytest.fixture()
+def masked_card_number():
+    return "0123 45** **** 3456"
+
+
+@pytest.fixture()
+def filtered_by_state():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+@pytest.fixture()
+def sorted_by_date():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '03.07.2019'}, {'id': 615064591, 'state': 'CANCELED', 'date': '14.10.2018'}, {'id': 594226727, 'state': 'CANCELED', 'date': '12.09.2018'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '30.06.2018'}]
+
+
+@pytest.fixture()
+def got_date():
+    return "11.03.2024"
