c8f5d3f053dbc74913c8537a543a106622724c55
Vlad <gromov.vlv@yandex.ru>
2025-10-11 15:49:25 +0300

    100% test coverage

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -4,18 +4,21 @@ from src.mask import get_mask_card_number, get_mask_account
 
 
 @pytest.mark.parametrize("value, expected", [
+    ("0123456789123456", "0123 45** **** 3456"),
+    ("0000000000000000000", "Некорректный ввод"),
+    ("11111111аа111111", "Некорректный ввод"),
+    ("342342", "Некорректный ввод")
+])
+def test_get_mask_card_number(value, expected):
+    assert get_mask_card_number(value) == expected
+
+
+@pytest.mark.parametrize("value, expected", [
     ("01234567891234567890", "**7890"),
-    ("56874956568899598899", "**8899"),
-    ("11111111111111111111", "**1111")
+    ("56874956568890009598899", "Некорректный ввод"),
+    ("111111111111111", "Некорректный ввод"),
+    ("43635635635ggg", "Некорректный ввод")
 ])
 def test_get_mask_account(value, expected):
     assert get_mask_account(value) == expected
 
-
-@pytest.mark.parametrize("value, expected", [
-    ("0123456789123456", "0123 45** **** 3456"),
-    ("0000000000000000", "0000 00** **** 0000"),
-    ("1111111111111111", "1111 11** **** 1111")
-])
-def test_get_mask_card_number(value, expected):
-    assert get_mask_card_number(value) == expected
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,8 +1,15 @@
+import pytest
+
 from src.widget import mask_account_card, get_date
 
 
-def test_mask_account_card():
-    assert mask_account_card("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
+@pytest.mark.parametrize("value, expected", [
+    ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+    ("Счет 01234567890123456789", "Счет **6789"),
+    ("Счёт 01234567890123456789", "Счёт **6789"),
+])
+def test_mask_account_card(value, expected):
+    assert mask_account_card(value) == expected
 
 def test_get_date(got_date):
     assert get_date("2024-03-11T02:26:18.671407") == got_date
