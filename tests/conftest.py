95ced1e9bc5ba2f7d61ea3f297bf8fdc3e82ffec
Vlad <gromov.vlv@yandex.ru>
2025-10-11 15:24:41 +0300

    parametrize created

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,6 @@ import pytest
 
 
 @pytest.fixture()
-def masked_account():
-    return "**7890"
-
-@pytest.fixture()
-def masked_card_number():
-    return "0123 45** **** 3456"
-
-
-@pytest.fixture()
 def filtered_by_state():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,9 +1,21 @@
+import pytest
+
 from src.mask import get_mask_card_number, get_mask_account
 
 
-def test_get_mask_account(masked_account):
-    assert get_mask_account ("01234567891234567890") == masked_account
+@pytest.mark.parametrize("value, expected", [
+    ("01234567891234567890", "**7890"),
+    ("56874956568899598899", "**8899"),
+    ("11111111111111111111", "**1111")
+])
+def test_get_mask_account(value, expected):
+    assert get_mask_account(value) == expected
 
 
-def test_get_mask_card_number(masked_card_number):
-    assert get_mask_card_number("0123456789123456") == masked_card_number
+@pytest.mark.parametrize("value, expected", [
+    ("0123456789123456", "0123 45** **** 3456"),
+    ("0000000000000000", "0000 00** **** 0000"),
+    ("1111111111111111", "1111 11** **** 1111")
+])
+def test_get_mask_card_number(value, expected):
+    assert get_mask_card_number(value) == expected
