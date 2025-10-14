81c41c4bd64694463094a09ce5f4d6531b638071
Vlad <gromov.vlv@yandex.ru>
2025-10-15 01:30:23 +0300

    generators tests created

--- src/generators.py
+++ src/generators.py
@@ -1,21 +1,31 @@
-def filter_by_currency(transactions: list[dict], currency_code: str) -> GeneratorExit(None):
+from typing import Generator
+
+
+def filter_by_currency(transactions: list[dict], currency_code: str) -> Generator[dict | str]:
     """Генератор. Возвращает из списка транзакций транзакцию с нужной ввалютой"""
+    no_information_message = "Нет информации о валюте"
     for transaction in transactions:
-        if transaction["operationAmount"]["currency"]["code"] == currency_code:
-            yield transaction
+        if "operationAmount" not in transaction:
+            yield no_information_message
+        elif transaction["operationAmount"]["currency"]["code"] == currency_code:
+            filtered_by_currency = transaction
+            yield filtered_by_currency
 
 
-def transaction_descriptions(transactions: list[dict]) -> GeneratorExit(None):
+def transaction_descriptions(transactions: list[dict]) -> Generator[str]:
     """Генератор. Возвращает описание транзакции"""
     for transaction in transactions:
-        yield transaction["description"]
+        if "description" not in transaction:
+            description = "Остутствует описание операции"
+        else:
+            description = transaction["description"]
+        yield description
 
 
-def card_number_generator(start: int, end: int) -> GeneratorExit(None):
+def card_number_generator(start: int, stop: int) -> Generator[str]:
     """Генератор. Возвращает номера карт в определенном диапазоне в формате XXXX XXXX XXXX XXXX"""
-
     str_zero = "0"
-    for number in range(start, end + 1):
+    for number in range(start, stop + 1):
         str_number = str(number)
         str_card_number = str_zero * (16 - len(str_number)) + str_number
         card_number = f"{str_card_number[0:4]} {str_card_number[4:8]} {str_card_number[8:12]} {str_card_number[12:16]}"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
+from typing import Generator
+
 import pytest
+
+from src.generators import filter_by_currency
 
 
 @pytest.fixture()
@@ -10,7 +14,7 @@ def filtered_by_state() -> list:
 
 
 @pytest.fixture()
-def sorted_by_date() -> list:
+def sorted_by_date() -> list[dict]:
     return [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '03.07.2019'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '14.10.2018'},
@@ -22,3 +26,37 @@ def sorted_by_date() -> list:
 @pytest.fixture()
 def got_date() -> str:
     return "11.03.2024"
+
+
+@pytest.fixture()
+def filter_by_currency_iterate() -> Generator[dict | str]:
+    filter_by_currency_list = [
+        {"id": 111111,
+         "state": "EXECUTED",
+         "date": "2018-06-30T02:08:58.425572",
+         "operationAmount": {
+             "amount": "9824.07", "currency": {"name": "USD", "code": "USD"}
+         },
+         "description": "Перевод организации № 1",
+         "from": "Счет 75106830613657916952",
+         "to": "Счет 11776614605963066702"
+         },
+        {"id": 222222,
+         "state": "EXECUTED",
+         "date": "2018-06-30T02:08:58.425572",
+         "operationAmount": {
+             "amount": "9824.07", "currency": {"name": "USD", "code": "USD"}
+         },
+         "description": "Перевод организации № 2",
+         "from": "Счет 75106830613657916952",
+         "to": "Счет 11776614605963066702"
+         },
+        {"id": 333333,
+         "state": "EXECUTED",
+         "date": "2018-06-30T02:08:58.425572",
+         "from": "Счет 75106830613657916952",
+         "to": "Счет 11776614605963066702"
+         },
+        {}
+    ]
+    return filter_by_currency(filter_by_currency_list, "USD")
